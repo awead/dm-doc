c53022f93cfdcb2c3d9322cf27230fdcc52e276f
Adam Wead <amsterdamos@gmail.com>
2013-04-27 10:45:09 -0400

    Fretting diagrams

--- tables/calc.pl
+++ tables/calc.pl
@@ -12,18 +12,20 @@ my @lines = <FILE>;
 close FILE;
 chomp @lines;
 
-# Open out output file
+# Open output file
 open (OUT, ">", "calc.tex")
   or die "Can't open calc.tex file!\n";
 
+# Determine number of columns
 my @cols = split /,/, $lines[0];
+#print scalar(@cols) . "\n";
 
-print OUT <<"EOF";
-\\begin{sidewaystable}[htdp]
-\\tiny
-\\begin{center}
-EOF
+# start the latex code, orient sideways if more than 12 columns
+print OUT "\\begin{sidewaystable}[htdp]\n" if @cols > 14;
+print OUT "\\tiny\n";
+print OUT "\\begin{center}\n";
 
+# start the latex table with the same number of columns as our csv file
 print OUT "\\begin{tabular}{";
 for (my $c = 0; $c < @cols; $c++) {
   if ($c == 0) {
@@ -34,19 +36,24 @@ for (my $c = 0; $c < @cols; $c++) {
   }
 }
 print OUT "}\n";
+
+# add a horizontal line at the top of the table
 print OUT "\\hline\n";
 
-
+# start printing out the csv data
 my $first = 1;
 foreach my $line (@lines) {
 
   my @fields = split /,/, $line;
+  
+  # first row has column names, so we'll put that in bold; normal text for the rest
   if ($first == 1) {
     print OUT "\\textbf{";
     print OUT join("} & \\textbf{", @fields);
     print OUT "} \\\\\n";
+    print OUT "\\hline\n";
   }
-  else {
+  else {  
     print OUT join(" & ", @fields) . " \\\\\n";
   } 
   $first = 0;
@@ -54,10 +61,10 @@ foreach my $line (@lines) {
 
 }
 
-print OUT <<"EOF";
-\\end{tabular}
-\\end{center}
-\\end{sidewaystable}
-EOF
+# finish out the latex code, return to normal sized text
+print OUT "\\end{tabular}\n";
+print OUT "\\end{center}\n";
+print OUT "\\end{sidewaystable}\n" if @cols > 14;
+print OUT "\\normalsize\n";
 
 close OUT;
